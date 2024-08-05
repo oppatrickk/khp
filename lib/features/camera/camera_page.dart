85e9efbfd8f1d5ffb6037c291d83e7a7eb41975f
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-08-06 04:03:23 +0800

    feat(calendar): created calendar page

--- lib/features/camera/camera_page.dart
+++ lib/features/camera/camera_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:khp/components/navigation/top_app_bar.dart';
 import 'package:khp/constants/app_colors.dart';
 
@@ -23,19 +25,86 @@ class CameraPage extends StatelessWidget {
             ),
           ),
           width: MediaQuery.of(context).size.width,
-          child: const Padding(
-            padding: EdgeInsets.all(32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'This is the camera page.',
-                  style: TextStyle(
-                    fontSize: 12,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Recent',
+                      style: TextStyle(
+                        color: AppColors.tertiary,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800,
+                        fontFamily: 'Mont',
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: AppColors.tertiary,
+                          width: 0.5,
+                        ),
+                        borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      ),
+                      height: 256,
+                      width: MediaQuery.of(context).size.width,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.landscape_rounded, size: 128),
+                          Text('No Recent Images'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    const Text(
+                      'Take your first picture now! Use the button below to get started.',
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: AppColors.surface,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24),
                   ),
                 ),
-              ],
-            ),
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FilledButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(AppColors.secondary),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.camera_rounded),
+                        SizedBox(width: 8),
+                        Text(
+                          'Use the Camera',
+                          style: TextStyle(
+                            fontFamily: 'Mont',
+                            fontWeight: FontWeight.w800,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),

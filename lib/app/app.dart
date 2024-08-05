daff80cebdb6a5cffb377c09309bc57aa7dc3c69
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-08-05 23:32:32 +0800

    chore(constants): initialized constants

---
+++ lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatefulWidget {
+  const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'KHP Sample Project',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  // * Non initable class
+  const AppColors._();
+
+  static const Color primary = Color(0xFF2451B6);
+  static const Color secondary = Color(0xFFF4C542);
+  static const Color tertiary = Color(0xFF2C3E50);
+  static const Color background = Color(0xFFE1E8F0);
+  static const Color surface = Color(0xFFF0F4FA);
+}

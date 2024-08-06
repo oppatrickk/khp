548f0046f366add42ffdbfb78de46151d6c5785f
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-08-06 18:16:46 +0800

    feat(gallery): added gallery page

--- lib/features/gallery/gallery_page.dart
+++ lib/features/gallery/gallery_page.dart
@@ -23,19 +23,70 @@ class GalleryPage extends StatelessWidget {
             ),
           ),
           width: MediaQuery.of(context).size.width,
-          child: const Padding(
-            padding: EdgeInsets.all(32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'This is the gallery page.',
-                  style: TextStyle(
-                    fontSize: 12,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Images',
+                      style: TextStyle(
+                        color: AppColors.tertiary,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800,
+                        fontFamily: 'Mont',
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: AppColors.tertiary,
+                          width: 0.5,
+                        ),
+                        borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      ),
+                      height: 256,
+                      width: MediaQuery.of(context).size.width,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.landscape_rounded, size: 128),
+                          Text('No Saved Images Yet'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    const Center(
+                      child: Text(
+                        'Here you can see all the pictures you took.',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Center(
+                child: FilledButton(
+                  onPressed: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'Take a Picture',
+                      style: TextStyle(
+                        fontFamily: 'Mont',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 12,
+                      ),
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
